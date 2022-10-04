1b08c6a9a93eabe775774dc0a80f17b4d10d5f15
Documenter.jl <documenter@juliadocs.github.io>
2022-10-04 23:48:55 +0000

    build based on 7ed60f0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FlipPA","category":"page"},{"location":"#FlipPA","page":"Home","title":"FlipPA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FlipPA.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FlipPA]","category":"page"}]
+}
