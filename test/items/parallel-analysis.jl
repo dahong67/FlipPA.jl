ed5aee6c2f92847fe3dc1a3737045da6dc7c73ee
David Hong <dahong67@wharton.upenn.edu>
2023-02-09 17:38:16 -0700

    Split test items into separate files

---
+++ test/items/parallel-analysis.jl
@@ -0,0 +1,41 @@
+## Parallel analysis methods
+
+@testset "Parallel analysis methods" begin
+    @testset "Config" begin
+        @testset "quantile=$quantile, trials=$trials, cmp=$cmp, seed=$seed" for quantile in 0:0.1:1, trials in [1, 10, 100], cmp in [Pairwise(), UpperEdge()], seed in 0:3
+
+            rng = StableRNG(seed)
+            @test PAconfig(quantile, trials, cmp, rng) isa PAconfig{Float64,Int64,typeof(cmp),StableRNGs.LehmerRNG}
+        end
+    end
+
+    @testset "flippa" begin
+        @testset "seed=$seed, signalrank=$k" for seed in 0:2, k in 0:2
+            rng = StableRNG(seed)
+            n, p = 60, 40
+
+            S = 4 * randn(rng, p, k) * randn(rng, k, n) / sqrt(p * n)
+            N = randn(rng, p, n) / sqrt(n)
+            X = S + N
+
+            @test flippa(X; rng=StableRNG(seed), trials=20) == k
+            @test flippa(X; rng=StableRNG(seed), trials=20, quantile=1) == flippa(X; rng=StableRNG(seed), trials=20, quantile=1.0)
+            @test flippa(X; rng=StableRNG(seed), trials=20, quantile=0) == flippa(X; rng=StableRNG(seed), trials=20, quantile=0.0)
+        end
+    end
+
+    @testset "permpa" begin
+        @testset "seed=$seed, signalrank=$k" for seed in 0:2, k in 1:2
+            rng = StableRNG(seed)
+            n, p = 60, 40
+
+            S = 4 * randn(rng, p, k) * randn(rng, k, n) / sqrt(p * n)
+            N = randn(rng, p, n) / sqrt(n)
+            X = S + N
+
+            @test permpa(X; rng=StableRNG(seed), trials=20) == k
+            @test permpa(X; rng=StableRNG(seed), trials=20, quantile=1) == permpa(X; rng=StableRNG(seed), trials=20, quantile=1.0)
+            @test permpa(X; rng=StableRNG(seed), trials=20, quantile=0) == permpa(X; rng=StableRNG(seed), trials=20, quantile=0.0)
+        end
+    end
+end
