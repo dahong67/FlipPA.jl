7baebab11ca598f42053f76c7c1d7a528cde7a1b
David Hong <hong@udel.edu>
2024-04-01 18:04:53 -0400

    Run formatter

--- src/FlipPA.jl
+++ src/FlipPA.jl
@@ -31,15 +31,24 @@ struct PAconfig{S<:AbstractFloat,T<:Integer,C<:ComparisonMethods,R<:AbstractRNG}
     cmp::C
     rng::R
 
-    function PAconfig{S,T,C,R}(quantile::S, trials::T, cmp::C, rng::R) where {S<:AbstractFloat,T<:Integer,C<:ComparisonMethods,R<:AbstractRNG}
+    function PAconfig{S,T,C,R}(
+        quantile::S,
+        trials::T,
+        cmp::C,
+        rng::R,
+    ) where {S<:AbstractFloat,T<:Integer,C<:ComparisonMethods,R<:AbstractRNG}
         zero(S) <= quantile <= one(S) ||
             throw(DomainError(quantile, "`quantile` must be within [0,1]."))
-        zero(T) < trials ||
-            throw(DomainError(trials, "`trials` must be positive."))
+        zero(T) < trials || throw(DomainError(trials, "`trials` must be positive."))
         return new{S,T,C,R}(quantile, trials, cmp, rng)
     end
 end
-PAconfig(quantile::S, trials::T, cmp::C, rng::R) where {S<:AbstractFloat,T<:Integer,C<:ComparisonMethods,R<:AbstractRNG} =
+PAconfig(
+    quantile::S,
+    trials::T,
+    cmp::C,
+    rng::R,
+) where {S<:AbstractFloat,T<:Integer,C<:ComparisonMethods,R<:AbstractRNG} =
     PAconfig{S,T,C,R}(quantile, trials, cmp, rng)
 
 # Signflipping
@@ -54,8 +63,16 @@ Estimate the signal rank of the data `X` using random signflips.
 + `comparison` : comparison method to use, `default = FlipPA.UpperEdge()`
 + `rng` : random number generator, `default = default_rng()`
 """
-flippa(X::AbstractMatrix; quantile=1.0, trials=100, comparison=UpperEdge(), rng=default_rng()) =
-    _flippa(X::AbstractMatrix, PAconfig(convert(AbstractFloat, quantile), trials, comparison, rng))
+flippa(
+    X::AbstractMatrix;
+    quantile = 1.0,
+    trials = 100,
+    comparison = UpperEdge(),
+    rng = default_rng(),
+) = _flippa(
+    X::AbstractMatrix,
+    PAconfig(convert(AbstractFloat, quantile), trials, comparison, rng),
+)
 function _flippa(X::AbstractMatrix, config::PAconfig)
     σ = svdvals(X)
     σt = [svdvals(X .* rand(config.rng, (-1, 1), size(X))) for _ in 1:config.trials]
@@ -74,11 +91,22 @@ Estimate the signal rank of the data `X` by permuting the entries of each column
 + `comparison` : comparison method to use, `default = FlipPA.UpperEdge()`
 + `rng` : random number generator, `default = default_rng()`
 """
-permpa(X::AbstractMatrix; quantile=1.0, trials=100, comparison=UpperEdge(), rng=default_rng()) =
-    _permpa(X::AbstractMatrix, PAconfig(convert(AbstractFloat, quantile), trials, comparison, rng))
+permpa(
+    X::AbstractMatrix;
+    quantile = 1.0,
+    trials = 100,
+    comparison = UpperEdge(),
+    rng = default_rng(),
+) = _permpa(
+    X::AbstractMatrix,
+    PAconfig(convert(AbstractFloat, quantile), trials, comparison, rng),
+)
 function _permpa(X::AbstractMatrix, config::PAconfig)
     σ = svdvals(X)
-    σt = [svdvals(mapslices(v -> shuffle(config.rng, v), X, dims=1)) for _ in 1:config.trials]
+    σt = [
+        svdvals(mapslices(v -> shuffle(config.rng, v), X; dims = 1)) for
+        _ in 1:config.trials
+    ]
     return _compare_svdvals(σ, σt, config.quantile, config.cmp)
 end
 
--- test/items/parallel-analysis.jl
+++ test/items/parallel-analysis.jl
@@ -4,10 +4,15 @@
     using FlipPA: PAconfig, Pairwise, UpperEdge
     using StableRNGs
 
-    @testset "quantile=$quantile, trials=$trials, cmp=$cmp, seed=$seed" for quantile in 0:0.1:1, trials in [1, 10, 100], cmp in [Pairwise(), UpperEdge()], seed in 0:3
+    @testset "quantile=$quantile, trials=$trials, cmp=$cmp, seed=$seed" for quantile in
+                                                                            0:0.1:1,
+        trials in [1, 10, 100],
+        cmp in [Pairwise(), UpperEdge()],
+        seed in 0:3
 
         rng = StableRNG(seed)
-        @test PAconfig(quantile, trials, cmp, rng) isa PAconfig{Float64,Int64,typeof(cmp),StableRNGs.LehmerRNG}
+        @test PAconfig(quantile, trials, cmp, rng) isa
+              PAconfig{Float64,Int64,typeof(cmp),StableRNGs.LehmerRNG}
     end
 end
 
@@ -22,9 +27,11 @@ end
         N = randn(rng, p, n) / sqrt(n)
         X = S + N
 
-        @test flippa(X; rng=StableRNG(seed), trials=20) == k
-        @test flippa(X; rng=StableRNG(seed), trials=20, quantile=1) == flippa(X; rng=StableRNG(seed), trials=20, quantile=1.0)
-        @test flippa(X; rng=StableRNG(seed), trials=20, quantile=0) == flippa(X; rng=StableRNG(seed), trials=20, quantile=0.0)
+        @test flippa(X; rng = StableRNG(seed), trials = 20) == k
+        @test flippa(X; rng = StableRNG(seed), trials = 20, quantile = 1) ==
+              flippa(X; rng = StableRNG(seed), trials = 20, quantile = 1.0)
+        @test flippa(X; rng = StableRNG(seed), trials = 20, quantile = 0) ==
+              flippa(X; rng = StableRNG(seed), trials = 20, quantile = 0.0)
     end
 end
 
@@ -39,8 +46,10 @@ end
         N = randn(rng, p, n) / sqrt(n)
         X = S + N
 
-        @test permpa(X; rng=StableRNG(seed), trials=20) == k
-        @test permpa(X; rng=StableRNG(seed), trials=20, quantile=1) == permpa(X; rng=StableRNG(seed), trials=20, quantile=1.0)
-        @test permpa(X; rng=StableRNG(seed), trials=20, quantile=0) == permpa(X; rng=StableRNG(seed), trials=20, quantile=0.0)
+        @test permpa(X; rng = StableRNG(seed), trials = 20) == k
+        @test permpa(X; rng = StableRNG(seed), trials = 20, quantile = 1) ==
+              permpa(X; rng = StableRNG(seed), trials = 20, quantile = 1.0)
+        @test permpa(X; rng = StableRNG(seed), trials = 20, quantile = 0) ==
+              permpa(X; rng = StableRNG(seed), trials = 20, quantile = 0.0)
     end
 end
